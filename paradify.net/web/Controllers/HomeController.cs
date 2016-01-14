2fcb6a6988ee1a875bafdce6e79a1c88ab34f228
volkanakinpasa <volkanakintr@gmail.com>
2016-01-14 23:46:49 +0100

    microsoft .net web & webapi

---
+++ paradify.net/web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
