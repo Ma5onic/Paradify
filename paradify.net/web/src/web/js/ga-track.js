5d37dd71871e94c5eb62a729c8f55bed34949ce8
Volkan Akın <volkanakintr@gmail.com>
2018-03-16 21:49:04 +0100

    remove pageview tracks on ga

--- paradify.net/web/src/web/js/ga-track.js
+++ paradify.net/web/src/web/js/ga-track.js
@@ -1,25 +1,25 @@
 var gaTrack = {
 
     addToPlaylistUrl: function () {
-        if (ga != undefined) {
-            ga('set', 'page', '/addToPlaylist' + location.search);
-            ga('send', 'pageview');
-        }
+        //if (ga != undefined) {
+        //    ga('set', 'page', '/addToPlaylist' + location.search);
+        //    ga('send', 'pageview');
+        //}
     },
 
     signout: function (userNane) {
-        if (ga != undefined) {
-            ga('set', 'page', '/signout/' + userNane);  
-            ga('send', 'pageview');
-        }
+        //if (ga != undefined) {
+        //    ga('set', 'page', '/signout/' + userNane);  
+        //    ga('send', 'pageview');
+        //}
     },
 
 
     paypalClick: function () {
-        if (ga != undefined) {
-            ga('set', 'page', '/paypal');
-                ga('send', 'pageview');
-        }
+        //if (ga != undefined) {
+        //    ga('set', 'page', '/paypal');
+        //        ga('send', 'pageview');
+        //}
     }
 }
 
